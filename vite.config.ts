886e084a91ac3599e351e527d898233000522a32
abhilashajha4 <153742431+abhilashajha4@users.noreply.github.com>
2025-10-07 15:01:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/curious-cloud-canvas/', // 
+  base: '/', // 
   server: {
     host: "::",
     port: 8080,
